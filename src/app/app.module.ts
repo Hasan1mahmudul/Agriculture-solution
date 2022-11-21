9b683e7be6497f72e745f319a904badd2bdfcbd2
Mahmudul Hasan <rakibsharif1999@gmail.com>
2022-11-21 18:58:13 +0600

    design updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
-
+import {FormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,8 @@ import { RegistrationComponent } from './registration/registration.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import {NgForm} from '@angular/forms'
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+
+   districts: string[] = ["Dhaka","Borishal", "Rajshahi", "Sylhet"];
+     registerData(data:any) {
+     console.log(data.number);
+     
+    
+   }
 
 }
