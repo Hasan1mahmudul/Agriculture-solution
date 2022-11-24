90975c2f1e61edc6ef9cc4c21badbcfe6f22441b
Mahmudul Hasan <rakibsharif1999@gmail.com>
2022-11-24 17:56:30 +0600

    logic updated

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {NgForm} from '@angular/forms'
 @Component({
   selector: 'app-registration',
@@ -6,12 +6,36 @@ import {NgForm} from '@angular/forms'
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+   isNameValid = true;
+   isNumberValid = true;
+   isDistrictValid = true;
+   selectedValues:any = "Select District";
+   @ViewChild('registerForm') form1: any;
 
    districts: string[] = ["Dhaka","Borishal", "Rajshahi", "Sylhet"];
      registerData(data:any) {
-     console.log(data.number);
+      console.log(data);
+      
+      this.form1.reset();
+      
+      
+     if(data.name.trim() == ""){
+       this.isNameValid = false;
+     }else{
+      this.isNameValid = true;
+     }
+     if(data.number.trim()  == ""){
+      this.isNumberValid = false;
+     }else{
+      this.isNumberValid = true;
+     }
+     if(data.districts == ""){
+      this.isDistrictValid = false;
+     }
+     else{
+      this.isDistrictValid = true;
+     }
      
-    
    }
 
 }
